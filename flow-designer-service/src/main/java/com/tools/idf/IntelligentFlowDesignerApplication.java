cad3dec6393d79f373a4aad56c95935b5bd91680
Omkar Ranade <omkar.ranade@amdocs.com>
2020-02-08 22:08:36 +0530

    initial checkin

---
+++ flow-designer-service/src/main/java/com/tools/idf/IntelligentFlowDesignerApplication.java
@@ -0,0 +1,13 @@
+package com.tools.idf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentFlowDesignerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelligentFlowDesignerApplication.class, args);
+	}
+
+}
---
+++ flow-designer-service/src/test/java/com/tools/idf/IntelligentFlowDesignerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tools.idf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelligentFlowDesignerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
